ff95754db83b94996ea6df03b9e73e79551f3e07
jose <jlguambo@gmail.com>
2022-05-07 23:50:54 -0500

    feat: Change Website title

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Gappnadero PWA
         </p>
         <a
           className="App-link"
