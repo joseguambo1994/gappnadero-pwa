9e15a6c52270c9c3f974b2dc7399b7f3c4b5ee1b
jose <jlguambo@gmail.com>
2022-05-07 23:47:00 -0500

    feat: Add firebase deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
-import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD5XCOuUbY0aPY3mKVBgy7CerCkLf9kQ_o",
+  authDomain: "gappnadero-pwa.firebaseapp.com",
+  projectId: "gappnadero-pwa",
+  storageBucket: "gappnadero-pwa.appspot.com",
+  messagingSenderId: "544904643491",
+  appId: "1:544904643491:web:d60b31d8dc598824fa1016",
+  measurementId: "G-04MWNQQY8P"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 function App() {
   return (
