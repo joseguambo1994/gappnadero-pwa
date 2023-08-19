3af7fed3698f6eed5083324de7f05825ac972260
Jose Guambo <jlguambo@gmail.com>
2023-08-19 00:04:26 -0500

    feat: Add cow creation connection to backend.

--- src/Pages/CowCreate/index.tsx
+++ src/Pages/CowCreate/index.tsx
@@ -1,9 +1,15 @@
 import { Box, Button, TextField } from '@mui/material';
 import './styles.css';
-import { Timestamp}  from "firebase/firestore";
+import { Timestamp, collection, addDoc}  from "firebase/firestore";
 import { useForm, SubmitHandler, Controller } from "react-hook-form"
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { DevTool } from "@hookform/devtools";
+import { db } from '../../firebase';
+import { useMutation } from 'react-query';
+import Link from '@mui/material/Link';
+import { useNavigate } from 'react-router-dom';
+import Lottie from "lottie-react";
+import Loading from "./loadingCow.json";
 
 
 
@@ -16,20 +22,55 @@ type Inputs = {
   lastHeat?: Timestamp,
 }
 
+const defaultValues = {
+  image: 'https://cdn.britannica.com/55/174255-050-526314B6/brown-Guernsey-cow.jpg',
+}
+
 const CowCreate =() => {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
     control
-  } = useForm<Inputs>()
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log("data", data)
+  } = useForm<Inputs>({defaultValues})
 
-  console.log(watch("lastHeat")) // watch input value by passing the name of it
+  const createCow = async (formData: Inputs) => {
+    const docRef = await addDoc(collection(db, "cattle"), formData);
+    console.log("Document written with ID: ", docRef.id);
+  }
+  const { isSuccess , isLoading, isError, error, mutate } = useMutation(createCow);
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    mutate({...data, weight: Number(data.weight)});
+  }
+
+  const navigate = useNavigate();
+  isSuccess && navigate("/cowList")
 
   return (
-    <Box sx={{backgroundColor:'white', p:2}}>
+    <div>
+      {isLoading ? (
+        <Box sx={{
+          width: 1,
+          height: 1,
+          backgroundColor: 'green'
+        }}
+        >
+          <Lottie animationData={Loading} />
+        </Box>
+      ) : (
+        <>
+          {isError ? (
+            <div>Ocurrio un error: {error?.toString()}</div>
+          ) : null}
+
+          {isSuccess ? <div>Registro de ganado creado
+            <Link href="/cowList" color="inherit">
+  {'color="inherit"'}
+</Link>
+          </div> : null}
+
+          <Box sx={{backgroundColor:'white', p:2}}>
       
       <TextField sx={{width: 1, mb:2}} label="Nombre" color="secondary"  
       error={errors.name ? true:false}
@@ -39,13 +80,10 @@ const CowCreate =() => {
       error={errors.name ? true:false}
       {...register("number", { required: true })}/>
 
-      <TextField sx={{width: 1, mb:2}} label="Peso" color="secondary"  
+      <TextField sx={{width: 1, mb:2}} label="Peso" color="secondary" 
+        type="number" 
       error={errors.name ? true:false}
       {...register("weight", { required: true })}/>
-
-      <TextField sx={{width: 1, mb:2}} label="Url de imagen" color="secondary"  
-      error={errors.name ? true:false}
-      {...register("image", { required: true })}/>
 
       <Controller 
           name="lastHeat"
@@ -57,6 +95,7 @@ const CowCreate =() => {
               label="Fecha de ultimo celo"
               sx={{width: 1, mb:2}}
               onChange={(e) => field.onChange(e)}
+              disableFuture
               />
           }
       />
@@ -79,6 +118,10 @@ const CowCreate =() => {
        onClick={handleSubmit(onSubmit)} variant="contained" >Crear</Button>
       <DevTool control={control} /> 
     </Box>
+        </>
+      )}
+    </div>
+  
   )
 }
 
